8ed1dcb9d2c658a43efa36a3459253d9f06fec1c
Souradip <dgsouro77@gmail.com>
2024-05-12 03:03:37 +0530

    Added toggle dark/light mode and completed the sidebar

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -1,12 +1,56 @@
 import React, { useState } from "react";
 import { Avatar, Box, Divider, IconButton, Switch } from "@mui/material"
 import { Outlet } from "react-router-dom";
-import { useTheme } from "@mui/material/styles";
+import { useTheme,styled } from "@mui/material/styles";
 import Logo from "../../assets/Images/logo.ico"
 import { Stack } from "@mui/material";
 import { Nav_Buttons } from "../../data";
 import { Gear } from "phosphor-react";
 import { faker } from "@faker-js/faker";
+import useSettings from "../../hooks/useSettings.js";
+
+
+const AntSwitch = styled(Switch)(({ theme }) => ({
+  width: 40,
+  height: 20,
+  padding: 0,
+  display: 'flex',
+  '&:active': {
+    '& .MuiSwitch-thumb': {
+      width: 15,
+    },
+    '& .MuiSwitch-switchBase.Mui-checked': {
+      transform: 'translateX(9px)',
+    },
+  },
+  '& .MuiSwitch-switchBase': {
+    padding: 2,
+    '&.Mui-checked': {
+      transform: 'translateX(20px)',
+      color: '#fff',
+      '& + .MuiSwitch-track': {
+        opacity: 1,
+        backgroundColor: theme.palette.mode === 'dark' ? '#177ddc' : '#1890ff',
+      },
+    },
+  },
+  '& .MuiSwitch-thumb': {
+    boxShadow: '0 2px 4px 0 rgb(0 35 11 / 20%)',
+    width: 16,
+    height: 16,
+    borderRadius: 8,
+    transition: theme.transitions.create(['width'], {
+      duration: 200,
+    }),
+  },
+  '& .MuiSwitch-track': {
+    borderRadius: 20 / 2,
+    opacity: 1,
+    backgroundColor:
+      theme.palette.mode === 'dark' ? 'rgba(255,255,255,.35)' : 'rgba(0,0,0,.25)',
+    boxSizing: 'border-box',
+  },
+}));
 
 const DashboardLayout = () => {
 
@@ -14,6 +58,8 @@ const DashboardLayout = () => {
   const [selected,setselected] = useState(0);
   
   console.log(theme);
+
+  const {onToggleMode}=useSettings();
   return (
     <>
       <Box
@@ -41,7 +87,7 @@ const DashboardLayout = () => {
          :<IconButton onClick={() =>  {
             setselected(el.index);
          }} 
-         sx={{width:"max-content",color:"black"}} key={el.index}>{el.icon}</IconButton>
+         sx={{width:"max-content",color: theme.palette.mode === "light" ? "black" : theme.palette.text.primary}} key={el.index}>{el.icon}</IconButton>
 
       ))}
       <Divider sx={{width:"50px"}}/>
@@ -57,15 +103,17 @@ const DashboardLayout = () => {
       :<IconButton onClick={() => {
         setselected(3);;
       }}
-      sx={{width:"max-content",color:"black"}} 
+      sx={{width:"max-content",color:theme.palette.mode === "light" ? "black":theme.palette.text.primary}} 
       >
       <Gear/>
     </IconButton>
     }
       </Stack>
       </Stack> 
-      <Stack>
-      <Switch defaultChecked />
+      <Stack spacing={4}>
+      <AntSwitch onChange={() => {
+        onToggleMode();
+      }}defaultChecked />
       <Avatar src={faker.image.avatar()} />
       </Stack>  
       </Stack>
